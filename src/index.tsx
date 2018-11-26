b67a7f2e1e02c47fe85269c306270646fbb2935a
Ben Woodward <ben@benwoodward.me.uk>
2018-11-26 22:09:47 +0000

    Move App things into App directory

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import 'react-app-polyfill/ie9';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
